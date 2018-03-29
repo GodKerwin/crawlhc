35cb1a534ffce8483b5d10c3dc4abe978e1bd503
lxu <lxu@landhightech.com>
2018-03-29 10:24:07 +0800

    更新解析方式 lxml -> html.parser

--- crawl/html_parser.py
+++ crawl/html_parser.py
@@ -7,33 +7,33 @@ from crawl import html_downloader
 
 class HtmlParser(object):
     def parse_first_floor(self, html_cont, root_url):
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         urls = soup.select('section.navBox > article.IndexNav > ul > li > a')
         list_url = urljoin(root_url, urls.pop(-1).get('href'))
         html_cont = html_downloader.HtmlDownloader().download(list_url)
         if html_cont is None:
             print('[parse_first_floor] skip because of 404! link[%s]' % list_url)
             return None
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         urls = soup.select('section > div.ListBox2 > dl > dd > a')
         result = [[url.text.replace('行业资讯', ''), urljoin(root_url, url.get('href'))] for url in urls]
         return result
 
     def parse_second_floor(self, html_cont, root_url):
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         urls = soup.select('section.navBox > article.hyIndexNav > ul > li > a')
         list_url = urljoin(root_url, urls.pop(-1).get('href'))
         html_cont = html_downloader.HtmlDownloader().download(list_url)
         if html_cont is None:
             print('[parse_second_floor] skip because of 404! link[%s]' % list_url)
             return None
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         urls = soup.select('section > div.ListBox2 > dl > dd > a')
         result = [[url.text, urljoin(root_url, url.get('href'))] for url in urls]
         return result
 
     def parse_third_floor(self, html_cont, root_url):
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         urls = soup.select('section.newsBox > ul > li.NewsListImg > h3 > a')
         urls2 = soup.select('section.newsBox > ul > li.NewsList > div.nListRight > h3 > a')
         urls3 = soup.select('section.newsBox > ul > li.NewsListImg > a')
@@ -47,7 +47,7 @@ class HtmlParser(object):
         return [urljoin(root_url, url.get('href')) for url in urls], next_url, has_next
 
     def parse_article(self, html_cont, root_url, pid, cid):
-        soup = BeautifulSoup(html_cont, 'lxml')
+        soup = BeautifulSoup(html_cont, 'html.parser')
         content = ''
         title = soup.select('section.dBox > div.dTopBox > h1')[0].text.strip()
         contents = soup.select('section.dBox > div.dCon > p')
